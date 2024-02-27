68eba11e1e9d718715114f9d7601cb7e0fe5cd88
Larissa Corrêa <larimscorrea@gmail.com>
2024-02-27 01:25:30 -0300

    quase finalizado

--- app/src/main/java/com/example/travel_costs_app_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/travel_costs_app_kotlin/MainActivity.kt
@@ -1,46 +1,45 @@
 package com.example.travel_costs_app_kotlin
 
+import android.app.Activity
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import android.widget.Toast
+import androidx.appcompat.app.AppCompactActivity
 import com.example.travel_costs_app_kotlin.ui.theme.Travelcostsapp_kotlinTheme
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity(), View.OnCLickListener {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            Travelcostsapp_kotlinTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
+    override fun onClick(view: View) {
+        if(view.id == R.id.button_Calculate)
+            calculate()
+    }
+
+    private fun isValid(): Boolean {
+        return (binding.editDistance.text.toString() != ""
+                && binding.editPrice.text.toString() != ""
+                && binding.editAutonomu.text.toString() != "")
+    }
+    
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+private fun calculate() {
+   if (isValid()) {
+       val distance = binding.editDistance.text.toString().toFloat()
+       val price = binding.editPrice.text.toString().toFloat()
+       val autonomy = binding.editAutonomy.text.toString().toFloat()
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Travelcostsapp_kotlinTheme {
-        Greeting("Android")
+       if (autonomy == Of) {
+           Toast.makeText(context this, R.string.no_div_raro, Toast.LENGTH_SHORT)
+       }
+
+   } else {
+       val totalValue = (price * distance)/autonomy
+       binding.textLabelResult.text = "U$: ${"%.2f".format(totalValue)}"
+   } else {
+       Toast.makeTex(content this, R.string.validatiam_fill_all_fields, Toast.LENGTH_SHORT)
     }
 }
